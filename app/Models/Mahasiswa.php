5edd45a1a6980da3f9d6cc98d8f91cbb1b91180f
elvirasania <elvirasania469@gmail.com>
2022-03-30 11:36:49 +0700

    Praktikum Jobsheet 7

---
+++ app/Models/Mahasiswa.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Mahasiswa extends Model
+{
+    use HasFactory;
+
+    protected $table = 'mahasiswa';// Eloquent akan membuat model mahasiswa menyimpan record di tabel mahasiswa
+    protected $primaryKey = 'id_mahasiswa';// Memanggil isi DB Dengan primarykey
+    
+    protected $fillable = [
+        'Nim',
+        'Nama',
+        'Kelas',
+        'Jurusan',
+    ]; 
+}
